271fd227476e7daf3798c4de3cd6f72891b04146
Domenico Capalbo <capalbodomenico@gmail.com>
2020-09-03 22:25:41 +0200

    css-html-complete-adder-other-4-dropdown
    js-html-css-revolut

--- js/main.js
+++ js/main.js
@@ -1,7 +1,27 @@
 $(document).ready(function () {
+  $("#tar").hover(
+  function() {
+    $(".specific-window").fadeToggle(1000);
+   }
+  );
+
+  $("#conti").hover(
+  function() {
+    $(".specific-window").fadeToggle(1000);
+  });
+
+  $("#prod").hover(
+  function() {
+    $(".specific-window").fadeToggle(1000);
+  });
+
   $("#window").hover(
   function() {
     $(".specific-window").fadeToggle(1000);
-  }
- );
+  });
+
+  $("#help").hover(
+  function() {
+    $(".specific-window").fadeToggle(1000);
+  });
 });
