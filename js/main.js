9b9c4c5891d0a404ead81211f0750f15caa9409c
Domenico Capalbo <capalbodomenico@gmail.com>
2020-09-03 23:15:07 +0200

    needs-refactory
    js-html-css-revolut

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+//1. make a function which show a dropdown at the hover of the mouse on one of the li inside of the html
+
   $("#tar").hover(
   function() {
     $(".specific-window-tariffazione").fadeToggle(200);
@@ -8,14 +10,17 @@ $(document).ready(function () {
   function() {
     $(".specific-window-conti").fadeToggle(200);
   });
+
   $("#prod").hover(
   function() {
     $(".specific-window-prodotti").fadeToggle(200);
   });
+
   $("#window").hover(
   function() {
     $(".specific-window-azienda").fadeToggle(200);
   });
+
   $("#help").hover(
   function() {
     $(".specific-window-aiuto").fadeToggle(200);
