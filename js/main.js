9219efa5a6e412885dd558a28d476c899b662c07
Domenico Capalbo <capalbodomenico@gmail.com>
2020-09-06 16:55:51 +0200

    exercise-complete-refactory-done
    js-html-css-revolut

--- js/main.js
+++ js/main.js
@@ -1,28 +1,7 @@
-$(document).ready(function () {
-//1. make a function which show a dropdown at the hover of the mouse on one of the li inside of the html
+$(document).ready(function() {
 
-  $("#tar").hover(
-  function() {
-    $(".specific-window-tariffazione").fadeToggle(200);
-   }
-  );
-  $("#conti").hover(
-  function() {
-    $(".specific-window-conti").fadeToggle(200);
+  $(".nav-menu-item").hover(function() {
+    $(this).children(".dropdown-menu").toggle();
   });
 
-  $("#prod").hover(
-  function() {
-    $(".specific-window-prodotti").fadeToggle(200);
-  });
-
-  $("#window").hover(
-  function() {
-    $(".specific-window-azienda").fadeToggle(200);
-  });
-
-  $("#help").hover(
-  function() {
-    $(".specific-window-aiuto").fadeToggle(200);
-  });
 });
